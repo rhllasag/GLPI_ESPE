0a4269a2928cafab1ba106e2d92eea0b0297403f
German Caicedo <capo2786@gmail.com>
2018-11-12 15:55:11 -0500

    adding connection files

---
+++ connection.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+	$host='10.1.0.113:1521/PAS8';
+	$usuario='UTIC';
+	$password='UT1C9090*';
+
+	$conn=oci_pconnect($usuario,$password,$host);
+    if (!$conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+	$query="SELECT UZGTPERSON_NOMBRE, UZGTPERSON_CORR, UZGTTELE_NUM_TELEFONO, UZGTEXTE_NUM_EXTENSION FROM BANINST1.BZPVGUIAT WHERE ID_TERCEROS='magallardo'";
+	//$query="SELECT UZGTPERSON_NOMBRE, UZGTPERSON_PUEST, UZGTPERSON_CORR,UZGTPERSON_CEDULA, UZGTTELE_NUM_TELEFONO, UZGTEXTE_NUM_EXTENSION FROM BANINST1.BZPVGUIAT WHERE ID_TERCEROS='".$this->fields['name']."'";
+	$resultado=oci_parse($conn,$query);
+
+	@oci_execute($resultado);
+	while (($row =oci_fetch_array($resultado))!=false){
+	$telefono_banner=$row['UZGTTELE_NUM_TELEFONO'];
+	$ext_banner=$row ['UZGTEXTE_NUM_EXTENSION'];
+	$n_banner= $row['UZGTPERSON_NOMBRE'];
+//	$n_cedula=$row ['UZGTPERSON_CEDULA'];
+//	$n_puesto=$row ['UZGTPERSON_PUEST'];
+	$val_correo=$row['UZGTPERSON_CORR'];
+	}
+	oci_close($conn);
+	echo "Ok";
+?>
---
+++ ftp.php
@@ -0,0 +1,25 @@
+﻿<?php
+$file = 'fromFTP.txt';
+$remote_file = 'C:/xampp/htdocs/fromFTP.txt';
+$ftp_server = "10.1.0.61";
+$ftp_user_name = "ftpglpi";
+$ftp_user_pass = "ftpglpi";
+
+
+// establecer una conexión básica
+$conn_id = ftp_connect($ftp_server);
+
+// iniciar sesión con nombre de usuario y contraseña
+$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+// cargar un archivo
+if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
+ echo "se ha cargado $file con éxito\n";
+} else {
+ echo "Hubo un problema durante la transferencia de $file\n";
+}
+
+// cerrar la conexión ftp
+ftp_close($conn_id);
+?>
+Ver tambié
